2f1e83a8d814d39d2c4fecfa8e71a1f1d20a4c9c
jesivira369 <eduardosivira17@gmail.com>
2024-03-04 18:40:18 -0300

    Update roles and create pipes for validator

---
+++ src/pipes/greater-zero/greater-zero.pipe.ts
@@ -0,0 +1,13 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class GreaterZeroPipe implements PipeTransform {
+  transform(value: any) {
+
+    if (isNaN(+value) || +value <= 0) {
+      return 1;
+    }
+
+    return value;
+  }
+}
